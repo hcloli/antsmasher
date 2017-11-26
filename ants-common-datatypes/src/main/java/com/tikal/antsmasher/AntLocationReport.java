fa0e5aa54371567fcee1cf58565396fde89f3f6a
David Peleg <davidpelfree@users.noreply.github.com>
2017-11-26 13:56:50 +0200

    Added ants-common-datatype

---
+++ ants-common-datatypes/src/main/java/com/tikal/antsmasher/AntLocationReport.java
@@ -0,0 +1,32 @@
+package com.tikal.antsmasher;
+
+import lombok.Builder;
+import lombok.Value;
+
+import java.util.Map;
+
+@Value
+@Builder
+public class AntLocationReport {
+
+    String type;
+    String address;
+    Message message;
+
+
+    @Value
+    @Builder
+    public static class Message {
+        Map<String, String> headers;
+        Body body;
+    }
+
+    @Value
+    @Builder
+    public static class Body {
+        String id;
+        String species;
+        int xPromil;
+        int yPromil;
+    }
+}
